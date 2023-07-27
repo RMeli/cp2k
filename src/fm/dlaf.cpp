5aa221ca22be0f599b7c2867dafb30b9e6fadb48
Rocco Meli <r.meli@bluemail.ch>
2023-07-27 08:58:39 +0200

    print pika binding

--- src/fm/dlaf.cpp
+++ src/fm/dlaf.cpp
@@ -8,31 +8,10 @@
 // SPDX-License-Identifier: BSD-3-Clause
 //
 
-#include <dlaf/init.h>
-#include <pika/execution.hpp>
-#include <pika/init.hpp>
-#include <pika/program_options.hpp>
-#include <pika/runtime.hpp>
-
-static bool dlaf_init_ = false;
+#include <dlaf_c/init.h>
 
 extern "C" void dlaf_init() {
-  if (!dlaf_init_) {
-    int argc = 1;
-    const char *const argv[] = {"cp2k", nullptr};
-
-    pika::program_options::options_description desc("cp2k");
-    desc.add(dlaf::getOptionsDescription());
-
-    /* pika initialization */
-    pika::init_params p;
-    p.rp_callback = dlaf::initResourcePartitionerHandler;
-    p.desc_cmdline = desc;
-    pika::start(nullptr, argc, argv, p);
-
-    /* DLA-Future initialization */
-    dlaf::initialize(argc, argv);
-    dlaf_init_ = true;
-    pika::suspend();
-  }
+  const char* pika_argv[] = {"cp2k", "--pika:print-bind"};
+  const char* dlaf_argv[] = {"cp2k"};
+  dlaf_initialize(2, pika_argv, 1, dlaf_argv);
 }
