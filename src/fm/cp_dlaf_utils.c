803651d1f286d016292604482ef26b7bfde99ea1
Rocco Meli <r.meli@bluemail.ch>
2023-08-03 15:57:55 +0200

    add missing licence from c file

--- src/fm/cp_dlaf_utils.c
+++ src/fm/cp_dlaf_utils.c
@@ -1,11 +1,18 @@
+/*----------------------------------------------------------------------------*/
+/*  CP2K: A general program to perform molecular dynamics simulations         */
+/*  Copyright 2000-2023 CP2K developers group <https://cp2k.org>              */
+/*                                                                            */
+/*  SPDX-License-Identifier: GPL-2.0-or-later                                 */
+/*----------------------------------------------------------------------------*/
+
 #if defined(__DLAF)
 
 #include <dlaf_c/init.h>
 
 void dlaf_init() {
-  const char *pika_argv[] = {"cp2k", "--pika:print-bind"};
+  const char *pika_argv[] = {"cp2k", "--pika:bind=none", "--pika:threads=12"};
   const char *dlaf_argv[] = {"cp2k"};
-  dlaf_initialize(2, pika_argv, 1, dlaf_argv);
+  dlaf_initialize(3, pika_argv, 1, dlaf_argv);
 }
 
 #endif // __DLAF
