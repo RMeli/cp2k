9ad199f3aa4caffb57c4b120d3323b44a58bbf33
Rocco Meli <r.meli@bluemail.ch>
2023-08-07 17:34:47 +0200

    refactoring

--- src/fm/cp_dlaf_utils.c
+++ src/fm/cp_dlaf_utils.c
@@ -7,12 +7,20 @@
 
 #if defined(__DLAF)
 
+#include <dlaf_c/eigensolver/eigensolver.h>
 #include <dlaf_c/init.h>
 
 void dlaf_init() {
   const char *pika_argv[] = {"cp2k", "--pika:bind=none", "--pika:threads=12"};
   const char *dlaf_argv[] = {"cp2k"};
   dlaf_initialize(3, pika_argv, 1, dlaf_argv);
+}
+
+void dlaf_pdsyevd_wrapper(int n, double *a, int ia, int ja, int desca[9],
+                          double *w, double *z, int iz, int jz, int descz[9],
+                          int *info) {
+  char uplo = 'L'; // Only lower triangular matrices are supported
+  dlaf_pdsyevd(uplo, n, a, ia, ja, desca, w, z, iz, jz, descz, info);
 }
 
 #endif // __DLAF
